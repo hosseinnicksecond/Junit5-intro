42fbf76358e8f12811ed56eb2204ef4168603b3f
hossein nick <hossinnick62@gmail.com>
2020-10-26 19:17:11 +0330

    add Before anf after method

--- src/test/java/home/train/GreetingTest.java
+++ src/test/java/home/train/GreetingTest.java
@@ -1,20 +1,41 @@
 package home.train;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
+    private Greeting greeting;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before -- from Before ALL");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("before each class");
+        greeting= new Greeting();
+    }
+
     @Test
     void helloWorld() {
-        Greeting greeting= new Greeting();
         System.out.println(greeting.helloWorld());
     }
 
     @Test
     void testHelloWorld() {
-        Greeting greeting=new Greeting();
         System.out.println(greeting.helloWorld("Hossein"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("after each class");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After-- from after all");
     }
 }
